712c4988c03eef292e3cdc936ef6f311483a8580
Vasilii Lysenkov <lysenkov2@ya.ru>
2021-08-19 22:12:15 +0300

    Added .gitignore and code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 99985;
+        int proportion = 20;
+        int miles = price / proportion;
+        System.out.println(miles);
+    }
+}
